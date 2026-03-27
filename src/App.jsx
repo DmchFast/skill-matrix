39f641671f0e7f14e075a86a9e48396a8a20eeda
DmchFast <chdima28@gmail.com>
2026-03-28 01:46:24 +0300

    feat: add React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
+import Home from './pages/Home';
 
 function App() {
-  return <Layout />;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+   return (
+      <div style={{ padding: '24px' }}>
+         <h1>Главная страница</h1>
+         <p>Добро пожаловать в SkillsMatrix!</p>
+      </div>
+   );
+};
+
+export default Home;
